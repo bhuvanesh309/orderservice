f64f0450f2b90960f9d15b775306524b28599d3f
BHUVANESWARAN <mgravi70@gmail.com>
2024-10-15 18:07:18 +0530

    this is for orderservice

---
+++ orderservice/src/main/java/com/tns/orderservice/OrderController.java
@@ -0,0 +1,61 @@
+package com.tns.orderservice;
+
+
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable int id) {
+        Order order = orderService.getOrderById(id);
+        if (order != null) {
+            return ResponseEntity.ok(order);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.saveOrder(order);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable int id, @RequestBody Order orderDetails) {
+        Order order = orderService.getOrderById(id);
+        if (order != null) {
+            order.setCustomerName(orderDetails.getCustomerName());
+            order.setProduct(orderDetails.getProduct());
+            order.setQuantity(orderDetails.getQuantity());
+            Order updatedOrder = orderService.updateOrder(order);
+            return ResponseEntity.ok(updatedOrder);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable int id) {
+        Order order = orderService.getOrderById(id);
+        if (order != null) {
+            orderService.deleteOrder(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
